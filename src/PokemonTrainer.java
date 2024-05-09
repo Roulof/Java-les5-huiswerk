7d1de6ab63d2f44b99a2afc37f8f74881dbbd8f2
Roulof <Roelo.school@gmail.com>
2024-05-09 10:55:35 +0200

    PokemonGym inteface toegevoegd

--- src/PokemonTrainer.java
+++ src/PokemonTrainer.java
@@ -3,17 +3,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PokemonTrainer {
-    {
-        private final String name;
-        private List<Pokemon> pokemons;
+
+    private final String name;
+    private List<Pokemon> pokemons;
 
     public PokemonTrainer(String name, List<Pokemon> pokemons) {
+        this.pokemons = pokemons;
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public List<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    public void setPokemons(List<Pokemon> pokemons) {
         this.pokemons = pokemons;
     }
 
-        public String getName() { return name; }
-        public List<Pokemon> getPokemons() { return pokemons; }
-    }
 }
-
